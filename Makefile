911e80276ce989ffdbe9bfd3ef13128f93d2c3ff
Merlijn Wajer <merlijn@wizzup.org>
2012-04-02 18:21:01 +0200

    Experimental OpenGL Frontend.

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: default all
 
-CFLAGS += -ansi -Werror -Wall -Wextra
+CFLAGS += -ansi -Werror -Wall -Wextra -ggdb
 CFLAGS += -I mupdf/fitz -I mupdf/pdf #-Ixps -Icbz -Iscripts
-LIBS += -lfreetype -ljbig2dec -ljpeg -lopenjpeg -lz -lm
+LIBS += -lfreetype -ljbig2dec -ljpeg -lopenjpeg -lz -lm -lGL -lSDL -lGLU -lSDL_image
 
 default: all
 	$(CC) least.c $(CFLAGS) $(LIBS) -o least mupdf/build/debug/libfitz.a
--- least.c
+++ least.c
@@ -1,5 +1,18 @@
+#include <SDL/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
 #include "mupdf/fitz/fitz.h"
 #include "mupdf/pdf/mupdf.h"
+
+static float w = 1920.0f;
+static float h = 1080.0f;
+float imw, imh;
+GLuint *pages;
+unsigned int pagec;
+
+static int pixmap_to_texture(void *pixmap, int width, int height, int format, int type);
+
 
 int open_pdf(fz_context *context, char *filename) {
     fz_stream *file;
@@ -11,8 +24,9 @@ int open_pdf(fz_context *context, char *filename) {
     fz_bbox bbox;
     fz_matrix ctm;
     int faulty;
-    int i, res;
-    char * s;
+    unsigned int i;
+    int res;
+    /* char * s; */
 
     faulty = 0;
 
@@ -39,7 +53,11 @@ int open_pdf(fz_context *context, char *filename) {
 
     ctm = fz_scale(res / 72.0f, res / 72.0f);
 
-    for(i = 0; i < fz_count_pages(doc); i++) {
+    /* XXX error handling */
+    pagec = fz_count_pages(doc);
+    pages = malloc(sizeof(unsigned int) * pagec);
+
+    for(i = 0; i < pagec; i++) {
         printf("Rendering page %d\n", i);
         page = fz_load_page(doc, i);
 
@@ -55,13 +73,18 @@ int open_pdf(fz_context *context, char *filename) {
         fz_run_page(doc, page, dev, ctm, NULL);
 
         /* Draw onto pixmap here */
+        pages[i] = pixmap_to_texture((void*)fz_pixmap_samples(context, image),
+                fz_pixmap_width(context, image),
+                fz_pixmap_height(context, image), 0, 0);
 
+        /*
         s = malloc(sizeof(char) * 20);
         sprintf(s, "/tmp/test%d.png", i);
 
         printf("Saving page %d to %s\n", i, s);
         fz_write_png(context, image, s, 0);
         free(s);
+        */
 
         fz_drop_pixmap(context, image);
 
@@ -78,6 +101,240 @@ int open_pdf(fz_context *context, char *filename) {
     return 0;
 }
 
+static int pixmap_to_texture(void *pixmap, int width, int height, int format, int type)
+{
+    unsigned int texname;
+
+    format = 42;
+    type = 31337;
+
+    glGenTextures(1, &texname);
+
+    /* Bind the texture object */
+    glBindTexture(GL_TEXTURE_2D, texname);
+
+    /* Set the texture's stretching properties */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+            GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+            GL_LINEAR);
+
+    /* Edit the texture object's image data using the information SDL_Surface gives us */
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width,
+             height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+             pixmap);
+
+    imw = width;
+    imh = height;
+
+    return texname;
+}
+
+static void quit_tutorial(int code)
+{
+	glDeleteTextures(pagec, pages);
+
+	exit(code);
+}
+
+static void handle_key_down(SDL_keysym * keysym)
+{
+	switch (keysym->sym) {
+	case SDLK_ESCAPE:
+		quit_tutorial(0);
+		break;
+	default:
+		break;
+	}
+
+}
+
+
+static void setup_opengl(int width, int height)
+{
+	/* float ratio = (float)width / (float)height; */
+
+	/* Our shading model--Gouraud (smooth). */
+	glShadeModel(GL_SMOOTH);
+
+	glEnable(GL_TEXTURE_2D);
+
+	/* Set the clear color. */
+	glClearColor(0, 0, 0, 0);
+
+	/* Setup our viewport. */
+	glViewport(0, 0, width, height);
+
+	glLoadIdentity();
+}
+
+int setup_sdl(void)
+{
+	/* Information about the current video settings. */
+	const SDL_VideoInfo *info = NULL;
+	/* Dimensions of our window. */
+	int width = 0;
+	int height = 0;
+	/* Color depth in bits of our window. */
+	int bpp = 0;
+	/* Flags we will pass into SDL_SetVideoMode. */
+    int flags = 0;
+
+	/* First, initialize SDL's video subsystem. */
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		/* Failed, exit. */
+		fprintf(stderr, "Video initialization failed: %s\n",
+			SDL_GetError());
+		quit_tutorial(1);
+	}
+
+	/* Let's get some video information. */
+	info = SDL_GetVideoInfo();
+
+	if (!info) {
+		/* This should probably never happen. */
+		fprintf(stderr, "Video query failed: %s\n", SDL_GetError());
+		quit_tutorial(1);
+	}
+
+	/*
+	 * Set our width/height to 640/480 (you would
+	 * of course let the user decide this in a normal
+	 * app). We get the bpp we will request from
+	 * the display. On X11, VidMode can't change
+	 * resolution, so this is probably being overly
+	 * safe. Under Win32, ChangeDisplaySettings
+	 * can change the bpp.
+	 */
+	width = (int)w;
+	height = (int)h;
+	bpp = info->vfmt->BitsPerPixel;
+
+	/*
+	 * Now, we want to setup our requested
+	 * window attributes for our OpenGL window.
+	 * We want *at least* 5 bits of red, green
+	 * and blue. We also want at least a 16-bit
+	 * depth buffer.
+	 *
+	 * The last thing we do is request a double
+	 * buffered window. '1' turns on double
+	 * buffering, '0' turns it off.
+	 *
+	 * Note that we do not use SDL_DOUBLEBUF in
+	 * the flags to SDL_SetVideoMode. That does
+	 * not affect the GL attribute state, only
+	 * the standard 2D blitting setup.
+	 */
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	/* flags = SDL_OPENGL | SDL_FULLSCREEN; */
+	flags = SDL_OPENGL;
+
+	if (SDL_SetVideoMode(width, height, bpp, flags) == 0) {
+		/* 
+		 * This could happen for a variety of reasons,
+		 * including DISPLAY not being set, the specified
+		 * resolution not being available, etc.
+		 */
+		fprintf(stderr, "Video mode set failed: %s\n", SDL_GetError());
+		quit_tutorial(1);
+	}
+
+
+	SDL_SetVideoMode(width, height, bpp, flags);
+
+	/*
+	 * EXERCISE:
+	 * Record timings using SDL_GetTicks() and
+	 * and print out frames per second at program
+	 * end.
+	 */
+
+	/* Never reached. */
+	return 0;
+}
+
+static void process_events(void)
+{
+	/* Our SDL event placeholder. */
+	SDL_Event event;
+
+	/* Grab all the events off the queue. */
+	while (SDL_PollEvent(&event)) {
+
+		switch (event.type) {
+		case SDL_KEYDOWN:
+			/* Handle key presses. */
+			handle_key_down(&event.key.keysym);
+			break;
+		case SDL_QUIT:
+			/* Handle quit requests (like Ctrl-c). */
+			quit_tutorial(0);
+			break;
+		}
+
+	}
+
+}
+
+static void draw_screen(void)
+{
+    int i;
+
+	glEnable(GL_TEXTURE_2D);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glViewport(0, 0, (int)w, (int)h);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0f, (int)w, (int)h, 0.0f, -1.0f, 1.0f);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+
+    for(i = 0; i < 2; i++) {
+        glBindTexture(GL_TEXTURE_2D, pages[i]);
+        /* Send our triangle data to the pipeline. */
+        glBegin(GL_QUADS);
+
+        /* Bottom-left vertex (corner) */
+        glTexCoord2i(0, 0);
+        glVertex3f(0.f, 0.f, 0.0f);
+
+        /* Bottom-right vertex (corner) */
+        glTexCoord2i(1, 0);
+        glVertex3f(imw, 0.f, 0.f);
+
+        /* Top-right vertex (corner) */
+        glTexCoord2i(1, 1);
+        glVertex3f(imw, imh, 0.f);
+
+        /* Top-left vertex (corner) */
+        glTexCoord2i(0, 1);
+        glVertex3f(0.f, imh, 0.f);
+
+        glEnd();
+        glTranslatef(imw, 0.f, 0.f);
+    }
+
+	/*
+	 * Swap the buffers. This this tells the driver to
+	 * render the next frame from the contents of the
+	 * back-buffer, and to set all rendering operations
+	 * to occur on what was the front-buffer.
+	 *
+	 * Double buffering prevents nasty visual tearing
+	 * from the application drawing on areas of the
+	 * screen that are being updated at the same time.
+	 */
+	SDL_GL_SwapBuffers();
+}
+
 int main (int argc, char **argv) {
     fz_context *context;
 
@@ -85,8 +342,31 @@ int main (int argc, char **argv) {
     if (!context)
         fprintf(stderr, "Failed to create context\n");
 
-    if (argc == 2)
+    if (argc == 2) {
+        /* Initialize OpenGL window */
+        setup_sdl();
+
+        /*
+         * At this point, we should have a properly setup
+         * double-buffered window for use with OpenGL.
+         */
+        setup_opengl(imw, imh);
+
+        /* Load textures from PDF file */
         open_pdf(context, argv[1]);
+
+        /*
+         * Now we want to begin our normal app process--
+         * an event loop with a lot of redrawing.
+         */
+        while (1) {
+            /* Process incoming events. */
+            process_events();
+            /* Draw the screen. */
+            draw_screen();
+        }
+
+    }
 
 
     fz_free_context(context);
